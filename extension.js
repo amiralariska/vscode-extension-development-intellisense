aa373e93d711cf1596ce53235d1d658127f71421
Amiral Ariska <ariskaamiral@gmail.com>
2025-02-19 21:26:05 +0800

    Fix snippet formatting for launch configuration in VS Code extension and "${workspaceFolder}" string for the workspace folder

--- extension.js
+++ extension.js
@@ -8,25 +8,25 @@ function activate(context) {
 	const launchjsonautocomplete = vscode.languages.registerCompletionItemProvider('jsonc', {
 		provideCompletionItems() {
 			const vscodelaunchjsoncompletionItem = new vscode.CompletionItem('VS Code Extension Development(Launch Extension)', vscode.CompletionItemKind.Module);
-			vscodelaunchjsoncompletionItem.insertText = new vscode.SnippetString(`	{
-        "name": "Launch Extension",
-        "type": "extensionHost",
-        "request": "launch",
-        "args": [
-            "--extensionDevelopmentPath=\${workspaceFolder}"
-        ]
-    }`);
+			vscodelaunchjsoncompletionItem.insertText = new vscode.SnippetString(`{
+	"name": "Launch Extension",
+	"type": "extensionHost",
+	"request": "launch",
+	"args": [
+		"--extensionDevelopmentPath=\\\${workspaceFolder\\}"
+	]
+}`);
 			vscodelaunchjsoncompletionItem.documentation = new vscode.MarkdownString("VS Code Extension Development");
 			vscodelaunchjsoncompletionItem.detail = 'VS Code Extension Development';
 			const vscodelaunchjsoncustomcompletionItem = new vscode.CompletionItem('VS Code Extension Development(Custom Launch Extension)', vscode.CompletionItemKind.Module);
-			vscodelaunchjsoncustomcompletionItem.insertText = new vscode.SnippetString(`	{
-        "name": "\${1:Launch Extension}",
-        "type": "extensionHost",
-        "request": "launch",
-        "args": [
-            "--extensionDevelopmentPath=\${workspaceFolder}"
-        ]
-    }$0`);
+			vscodelaunchjsoncustomcompletionItem.insertText = new vscode.SnippetString(`{
+	"name": "\${1:Launch Extension}",
+	"type": "extensionHost",
+	"request": "launch",
+	"args": [
+		"--extensionDevelopmentPath=\\\${workspaceFolder\\}"
+	]
+}$0`);
 			vscodelaunchjsoncustomcompletionItem.documentation = new vscode.MarkdownString("VS Code Extension Development(Custom)");
 			vscodelaunchjsoncustomcompletionItem.detail = 'VS Code Extension Development(Custom)';
 			return [
